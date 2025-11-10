96285c2ad45db42dccdd987eb5d659c3d8736859
sandhyareddy451 <vemula.sandhya51@gmail.com>
2025-11-10 13:05:59 +0530

    chainlit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    
+    
+    name ="Generative AI PRoject",
+    version = "0.0.0",
+    author = "sandhya",
+    author_email ="vemula.sandhya51@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+    
+)
